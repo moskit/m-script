e4403df4b902e9946335904ebb268f788d2a879a
Igor Simonov <me@igorsimonov.com>
2011-06-09 13:06:47 +0200

    Some fixes, route53query added

--- helpers/install_ec2_tools.sh
+++ helpers/install_ec2_tools.sh
@@ -32,10 +32,30 @@ EC2_HOME="/opt/ec2-tools"
 [ -d $EC2_HOME ] && rm -rf "$EC2_HOME/*" || install -d $EC2_HOME
 $WGET "http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip"
 $WGET "http://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.zip"
+$WGET "http://ec2-downloads.s3.amazonaws.com/ElasticLoadBalancing.zip"
 fullnameapi=`unzip -Z -1 ec2-api-tools.zip | tail -1 | awk -F'/' '{print $1}'`
-$UNZIP -qq -d /tmp ec2-api-tools.zip && cp -a "/tmp/$fullnameapi/*" $EC2_HOME
+$UNZIP -qq -d /tmp ec2-api-tools.zip
+if [ -d "/tmp/$fullnameapi" ] ; then
+  cp -a /tmp/$fullnameapi/* $EC2_HOME
+  rm -rf ec2-api-tools.zip
+else
+  echo "Unzip didn't work as expected. Keeping ec2-api-tools.zip in $PWD, please unzip it manually to $EC2_HOME"
+fi
 fullnameami=`unzip -Z -1 ec2-ami-tools.zip | tail -1 | awk -F'/' '{print $1}'`
-$UNZIP -qq -d /tmp ec2-ami-tools.zip && cp -a "/tmp/$fullnameami/*" $EC2_HOME
-
-rm -rf ec2-api-tools.zip ec2-ami-tools.zip /tmp/$fullnameapi /tmp/$fullnameami
+$UNZIP -qq -d /tmp ec2-ami-tools.zip
+if [ -d "/tmp/$fullnameami" ] ; then
+  cp -a /tmp/$fullnameami/* $EC2_HOME
+  rm -rf ec2-ami-tools.zip
+else
+  echo "Unzip didn't work as expected. Keeping ec2-ami-tools.zip in $PWD, please unzip it manually to $EC2_HOME"
+fi
+fullnameelb=`unzip -Z -1 ElasticLoadBalancing.zip | tail -1 | awk -F'/' '{print $1}'`
+$UNZIP -qq -d /tmp ElasticLoadBalancing.zip
+if [ -d "/tmp/$fullnameelb" ] ; then
+  cp -a /tmp/$fullnameelb/* $EC2_HOME
+  rm -rf ElasticLoadBalancing.zip
+else
+  echo "Unzip didn't work as expected. Keeping ec2-ami-tools.zip in $PWD, please unzip it manually to $EC2_HOME"
+fi
+rm -rf /tmp/$fullnameapi /tmp/$fullnameami /tmp/$fullnameelb 2>/dev/null
 
