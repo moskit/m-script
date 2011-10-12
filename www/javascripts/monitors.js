2652719bce4a0ac71c1c3c7e30323b85ee22b5a4
Igor Simonov <me@igorsimonov.com>
2011-10-12 18:28:02 +0200

    bits related to dashboard

--- www/javascripts/monitors.js
+++ www/javascripts/monitors.js
@@ -1,4 +1,6 @@
 init = function() {
+  el = document.body;
+  el.observe('click', hideAll);
   initMonitors();
 }
 
@@ -6,11 +8,52 @@ initMonitors = function() {
 
 }
 
-showData = function(theid,thetext,action) {
+showData = function(theid,thetext) {
   if ($(theid).style.display == "none") {
     $(theid).innerHTML=thetext;
     Effect.SlideDown(theid, {duration: 0.3});
 	}
+}
+
+showMenu = function(theid,thetext,action) {
+  if ($(theid).style.display == "none") {
+    $(theid).innerHTML=thetext;
+    Effect.SlideDown(theid, {duration: 0.3});
+    Event.observe($(theid), "keypress", function(e) {
+      var cKeyCode = e.keyCode || e.which;
+      if (cKeyCode == Event.KEY_RETURN) {
+        keyEvent(theid);
+        e.stop();
+      }
+    });
+	  
+	  if (action == 'gettags') {
+	    var the_path = $('path_' + theid).value;
+      if (the_path) {
+        var the_url = '/bin/gettags?path=' + escape(the_path);
+        new Ajax.Request(the_url, {
+          onComplete: function(response) {
+//            var result_array = response.responseText.split('\n');  
+            $('tags_' + theid).update(response.responseText);
+//            $('tags_' + theid).update(result_array[1]);
+          }
+        });
+      }
+	  }
+	}
+//	if (!e) var e = window.event;
+//  e.cancelBubble = true;
+//  if (e.stopPropagation) e.stopPropagation();
+}
+
+hideAll = function(e) {
+  var targ;
+	if (!e) var e = this.event;
+	if (e.target) targ = e.target;
+	else if (e.srcElement) targ = e.srcElement;
+//	if (targ.nodeType == 3) 
+  targ = targ.parentNode.parentNode;
+  $$('div.dhtmlmenu').each(function(value) { if (value.id != targ.id) value.hide(); });
 }
 
 
