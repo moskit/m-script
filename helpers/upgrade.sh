c9fb63796576f27093cbffeaa16cbde13101bb1d
Igor Simonov <me@igorsimonov.com>
2013-02-10 15:52:35 +0100

    bits

--- helpers/upgrade.sh
+++ helpers/upgrade.sh
@@ -43,28 +43,28 @@ find /tmp/m_script/.update -type d -name .git | xargs rm -rf
 echo "Checking directories:"
 for script in `find "/tmp/m_script/.update" -type d`; do
   printf "${script##*/} ... "
-  oldscript=`echo "$script" | sed "s|/tmp/m_script/.update/|${rpath}/../|"`
-  if [ ! -e "${oldscript}" ]; then
+  oldscript=`echo "$script" | sed "s|/tmp/m_script/.update/|$rpath/../|"`
+  if [ ! -e "$oldscript" ]; then
     printf "copying ... "
-    cp -r "${script}" "$oldscript" && chown -R `id -un`:`id -gn` "$oldscript" && echo "OK"
+    cp -r "$script" "$oldscript" && chown -R `id -un`:`id -gn` "$oldscript" && echo "OK"
   fi
 done
 echo "Checking files:"
 for script in `find "/tmp/m_script/.update" -type f`; do
   echo -n " -- ${script##*/} ... "
-  oldscript=`echo "$script" | sed "s|/tmp/m_script/.update/|${rpath}/../|"`
-  if [ -x "${oldscript}" ]; then
-    if [ "${script}" -nt "$oldscript" ]; then
-      cp "${script}" "$oldscript" && chown `id -un`:`id -gn` "$oldscript" && echo "OK"
+  oldscript=`echo "$script" | sed "s|/tmp/m_script/.update/|$rpath/../|"`
+  if [ -x "$oldscript" ]; then
+    if [ "$script" -nt "$oldscript" ]; then
+      cp "$script" "$oldscript" && chown `id -un`:`id -gn` "$oldscript" && echo "OK"
     else
       echo "This file is older than the local one. Not updated"
     fi
-  elif [ ! -e "${oldscript}" ]; then
+  elif [ ! -e "$oldscript" ]; then
     printf "new file. Copying ... "
-    cp "${script}" "$oldscript" && chown `id -un`:`id -gn` "$oldscript" && echo "OK"
-  elif [ "${script}" -nt "$oldscript" ]; then
+    cp "$script" "$oldscript" && chown `id -un`:`id -gn` "$oldscript" && echo "OK"
+  elif [ "$script" -nt "$oldscript" ]; then
     printf "this file is newer than the local one; saving as ${oldscript}.new, please check the differences manually ... "
-    cp "${script}" "${oldscript}.new" && chown `id -un`:`id -gn` "${oldscript}.new" && echo "OK"
+    cp "$script" "${oldscript}.new" && chown `id -un`:`id -gn` "${oldscript}.new" && echo "OK"
   else
     echo "not copying this file"
   fi
@@ -72,54 +72,58 @@ done
 echo "Checking symlinks:"
 for symlink in `find "/tmp/m_script/.update" -type l`; do
   echo -n " -- ${symlink##*/} ... "
-  oldsymlink=`echo "$symlink" | sed "s|/tmp/m_script/.update/|${rpath}/../|"`
-  cp -P "${symlink}" "$oldsymlink" && chown `id -un`:`id -gn` "$oldsymlink" && echo "OK"
+  oldsymlink=`echo "$symlink" | sed "s|/tmp/m_script/.update/|$rpath/../|"`
+  cp -P "$symlink" "$oldsymlink" && chown `id -un`:`id -gn` "$oldsymlink" && echo "OK"
 done
 printf "Removing .new files that have zero difference with the local files ..."
-for newfile in `find ${rpath}/../ -name "*.new"` ; do
+for newfile in `find "$rpath/../" -name "*.new"` ; do
   if [ `diff $newfile ${newfile%.new} | wc -l` -eq 0 ] ; then
     rm -f $newfile && touch ${newfile%.new} && printf "."
   fi
 done && echo "OK"
 printf "Running actions specific for this upgrade ... "
-if [ -f "${rpath}/../this_upgrade_actions" ] ; then
+if [ -f "$rpath/../this_upgrade_actions" ] ; then
   printf "found ... "
-  thists=`head -1 "${rpath}/../this_upgrade_actions" | sed 's|#||;s|[[:space:]]||'`
+  thists=`head -1 "$rpath/../this_upgrade_actions" | sed 's|#||;s|[[:space:]]||'`
   if [[ $thists =~ [^[0-9]] ]] ; then
     echo "Timestamp not found or has wrong format, 1st line of the script must be: # <epoch>"
   else
-    [ -f "${rpath}/../upgrade.log" ] || echo 0 > "${rpath}/../upgrade.log"
-    lastts=`tail -1 "${rpath}/../upgrade.log"`
+    [ -f "$rpath/../upgrade.log" ] || echo 0 > "$rpath/../upgrade.log"
+    lastts=`tail -1 "$rpath/../upgrade.log"`
     if [[ $lastts =~ [^[0-9]] ]] ; then
       echo "Unable to find last upgrade time, using file VERSION creation time"
       lastts=$(date -d "$(`which stat` -c %y VERSION | cut -d'.' -f1)" +"%s")
     fi
-    echo "timestamps: now $timeindex, last $lastts, this $thists, git $gitts" >> "${rpath}/../upgrade_actions.log"
+    echo "timestamps: now $timeindex, last $lastts, this $thists, git $gitts" >> "$rpath/../upgrade_actions.log"
     if [ `expr $thists \> $lastts` -eq 1 ] ; then
-      echo "`date` Running this upgrade specific actions script" >> "${rpath}/../upgrade_actions.log"
+      echo "`date` Running this upgrade specific actions script" >> "$rpath/../upgrade_actions.log"
       
-      bash "${rpath}/../this_upgrade_actions" ${rpath} >> "${rpath}/../upgrade_actions.log"
+      bash "$rpath/../this_upgrade_actions" "$rpath" >> "$rpath/../upgrade_actions.log"
       if [[ $? -eq 0 ]] ; then
         echo "OK"
-        install -d "${rpath}/../upgrade_actions"
-        mv "${rpath}/../this_upgrade_actions" "${rpath}/../upgrade_actions/${thists}.success"
+        install -d "$rpath/../upgrade_actions"
+        mv "$rpath/../this_upgrade_actions" "$rpath/../upgrade_actions/${thists}.success"
       else
-        install -d "${rpath}/../upgrade_actions"
-        mv "${rpath}/../this_upgrade_actions" "${rpath}/../upgrade_actions/${thists}.failed"
+        install -d "$rpath/../upgrade_actions"
+        mv "$rpath/../this_upgrade_actions" "$rpath/../upgrade_actions/${thists}.failed"
         echo "Error. Check the script: this_upgrade_actions.failed"
       fi
     else
       echo "outdated, not applying"
     fi
   fi
-  rm -f "${rpath}/../this_upgrade_actions"
+  rm -f "$rpath/../this_upgrade_actions"
 else
   echo "not found"
 fi
 rm -rf /tmp/m_script/.update/
-echo $timeindex >> "${rpath}/../upgrade.log"
+echo $timeindex >> "$rpath/../upgrade.log"
 
-echo -e "Showing files that are not updated and saved with the .new extension added. Note that configuration files are not shown! Use this to find all files if you wish:\n\nfind \"$M_ROOT\" -name \"*.new\"\n\nUse the unnew helper script to overwrite specific files with their new version:\n\nunnew </path/to/file1.new /path/to/file2.new ...>\n\n"
-find "$M_ROOT" -name "*.new" | grep -vE "\.conf\.|/conf/"
 
+if $fullupgrade ; then
+  find "$M_ROOT" -name "*.new" | grep -vE "\.conf\.|/conf/|\.list\." | while read updated ; do "$M_ROOT"/helpers/unnew $updated ; done
+else
+  echo -e "Showing files that are not updated and saved with the .new extension added. Note that configuration files are not shown! Use this to find all files if you wish:\n\nfind \"$M_ROOT\" -name \"*.new\"\n\nUse the unnew helper script to overwrite specific files with their new version:\n\nunnew </path/to/file1.new /path/to/file2.new ...>\n\n"
+  find "$M_ROOT" -name "*.new" | grep -vE "\.conf\.|/conf/|\.list\."
+fi
 
