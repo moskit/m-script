6c2ec0e74feed5efce4bca3e1c8249c6da14bda7
Igor Simonov <me@igorsimonov.com>
2013-09-01 21:28:17 +0200

    REST show_servers for EC2

--- lib/functions.sh
+++ lib/functions.sh
@@ -105,10 +105,10 @@ gendash() {
   [ -n "`grep '<\*\*\*>' "$1"`" ] && indic="w3"
   case $DASHBOARD in
     HTML)
-      "${fpath}/genhtml" --type=dash --css=${indic}${3} --folder="$2/localhost" "$1" 2>>"$M_ROOT/logs/dashboard.log"
+      "$fpath/genhtml" --type=dash --css=${indic}${3} --folder="$2/localhost" "$1" 2>>"$M_ROOT/logs/dashboard.log"
       ;;
     JSON)
-      "${fpath}/genjson" --type=dash --css=${indic}${3} --folder="$2/localhost" "$1" 2>>"$M_ROOT/logs/dashboard.log"
+      "$fpath/genjson" --type=dash --css=${indic}${3} --folder="$2/localhost" "$1" 2>>"$M_ROOT/logs/dashboard.log"
       ;;
   esac
 }
@@ -116,10 +116,10 @@ gendash() {
 genreport() {
   case $DASHBOARD in
     HTML)
-      "${fpath}/genhtml" --type=report --css=${indic}${3} --folder="$2/localhost" "$1" 2>>"$M_ROOT/logs/dashboard.log"
+      "$fpath/genhtml" --type=report --css=${indic}${3} --folder="$2/localhost" "$1" 2>>"$M_ROOT/logs/dashboard.log"
       ;;
     JSON)
-      "${fpath}/genjson" --type=report --css=${indic}${3} --folder="$2/localhost" "$1" 2>>"$M_ROOT/logs/dashboard.log"
+      "$fpath/genjson" --type=report --css=${indic}${3} --folder="$2/localhost" "$1" 2>>"$M_ROOT/logs/dashboard.log"
       ;;
   esac
 }
