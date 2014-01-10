d0fc3c3773d2895a8c3c5447c2dd10544569c341
Igor Simonov <me@igorsimonov.com>
2014-01-10 16:25:25 +0100

    fix

--- lib/dash_functions.sh
+++ lib/dash_functions.sh
@@ -119,7 +119,7 @@ print_inline() {
     dfpistyle=`echo "$dfpistatusarg" | cut -s -d'|' -f3`
     if [ -n "$dfpionclick" ]; then
       classadded="clickable"
-      onclick="onclick=\"showDetails('${dfocid}_name','$dfpionclick')\""
+      onclick="onclick=\"showDetails('${dfolid}_name','$dfpionclick')\""
     else
       unset onclick classadded dfpionclick
     fi
