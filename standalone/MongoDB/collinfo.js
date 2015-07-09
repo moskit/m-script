9210d858a9e1de3bbeb2270ef858f1e50515f2dd
Igor Simonov <me@igorsimonov.com>
2015-07-09 15:33:34 +0200

    setweb helper

--- standalone/MongoDB/collinfo.js
+++ standalone/MongoDB/collinfo.js
@@ -6,9 +6,14 @@ print('coll_count=' + objStatus.count);
 print('coll_size=' + objStatus.size);
 print('coll_storsize=' + objStatus.storageSize);
 print('coll_indexsize=' + objStatus.totalIndexSize);
-print('coll_sharded=' + objStatus.sharded);
 print('coll_capped=' + objStatus.capped);
-print('coll_chunks=' + objStatus.nchunks);
+if (objStatus.sharded) {
+  print('coll_sharded=' + objStatus.sharded);
+  print('coll_chunks=' + objStatus.nchunks);
+} else {
+  print('coll_sharded=false');
+  print('coll_chunks=0');
+}
 var indSizes = objStatus.indexSizes;
 for (var i in indSizes) {
   print('index_size+=("' + i + ':' + indSizes[i] + '")');
