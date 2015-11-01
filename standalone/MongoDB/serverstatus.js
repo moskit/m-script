b3ba370c97d1fb18b4b7a55f81eae9855ac6268c
Igor Simonov <me@igorsimonov.com>
2015-11-01 17:41:49 +0100

    MongoDB replication status

--- standalone/MongoDB/serverstatus.js
+++ standalone/MongoDB/serverstatus.js
@@ -1,41 +1,42 @@
 var objStatus = db.serverStatus()
-print('status=' + objStatus.ok);
-print('version=' + objStatus.version);
-print('uptime=' + objStatus.uptime);
-print('memRes=' + objStatus.mem.resident);
-print('memVir=' + objStatus.mem.virtual);
-print('memMapped=' + objStatus.mem.mapped);
-print('memMappedWJournal=' + objStatus.mem.mappedWithJournal);
-print('infoHeapUsage=' + objStatus.extra_info.heap_usage_bytes);
+if (objStatus.ok == 1) {
+  print('version=' + objStatus.version);
+  print('uptime=' + objStatus.uptime);
+  print('memRes=' + objStatus.mem.resident);
+  print('memVir=' + objStatus.mem.virtual);
+  print('memMapped=' + objStatus.mem.mapped);
+  print('memMappedWJournal=' + objStatus.mem.mappedWithJournal);
+  print('infoHeapUsage=' + objStatus.extra_info.heap_usage_bytes);
 
-print('connCurrent=' + objStatus.connections.current);
-print('connAvailable=' + objStatus.connections.available);
-if (objStatus.network) {
-  print('netIn=' + objStatus.network.bytesIn);
-  print('netOut=' + objStatus.network.bytesOut);
-  print('netReqn=' + objStatus.network.numRequests);
-}
-if (objStatus.opcounters) {
-  print('opcountersInsert=' + objStatus.opcounters.insert);
-  print('opcountersQuery=' + objStatus.opcounters.query);
-  print('opcountersUpdate=' + objStatus.opcounters.update);
-  print('opcountersDelete=' + objStatus.opcounters.delete);
-  print('opcountersGetmore=' + objStatus.opcounters.getmore);
-  print('opcountersCommand=' + objStatus.opcounters.command);
-}
-if (objStatus.globalLock) {
-  print('totalTime=' + objStatus.globalLock.totalTime);
-  print('lockTime=' + objStatus.globalLock.lockTime);
-  print('lockRatio=' + objStatus.globalLock.ratio);
-  print('lockQueueTotal=' + objStatus.globalLock.currentQueue.total);
-  print('lockQueueReaders=' + objStatus.globalLock.currentQueue.readers);
-  print('lockQueueWriters=' + objStatus.globalLock.currentQueue.writers);
-  print('lockClientsTotal=' + objStatus.globalLock.activeClients.total);
-  print('lockClientsReaders=' + objStatus.globalLock.activeClients.readers);
-  print('lockClientsWriters=' + objStatus.globalLock.activeClients.writers);
-}
-if (objStatus.recordStats) {
-  print('accessesNotInMemory=' + objStatus.recordStats.accessesNotInMemory);
-  print('pageFaultExceptionsThrown=' + objStatus.recordStats.pageFaultExceptionsThrown);
-}
+  print('connCurrent=' + objStatus.connections.current);
+  print('connAvailable=' + objStatus.connections.available);
+  if (objStatus.network) {
+    print('netIn=' + objStatus.network.bytesIn);
+    print('netOut=' + objStatus.network.bytesOut);
+    print('netReqn=' + objStatus.network.numRequests);
+  }
+  if (objStatus.opcounters) {
+    print('opcountersInsert=' + objStatus.opcounters.insert);
+    print('opcountersQuery=' + objStatus.opcounters.query);
+    print('opcountersUpdate=' + objStatus.opcounters.update);
+    print('opcountersDelete=' + objStatus.opcounters.delete);
+    print('opcountersGetmore=' + objStatus.opcounters.getmore);
+    print('opcountersCommand=' + objStatus.opcounters.command);
+  }
+  if (objStatus.globalLock) {
+    print('totalTime=' + objStatus.globalLock.totalTime);
+    print('lockTime=' + objStatus.globalLock.lockTime);
+    print('lockRatio=' + objStatus.globalLock.ratio);
+    print('lockQueueTotal=' + objStatus.globalLock.currentQueue.total);
+    print('lockQueueReaders=' + objStatus.globalLock.currentQueue.readers);
+    print('lockQueueWriters=' + objStatus.globalLock.currentQueue.writers);
+    print('lockClientsTotal=' + objStatus.globalLock.activeClients.total);
+    print('lockClientsReaders=' + objStatus.globalLock.activeClients.readers);
+    print('lockClientsWriters=' + objStatus.globalLock.activeClients.writers);
+  }
+  if (objStatus.recordStats) {
+    print('accessesNotInMemory=' + objStatus.recordStats.accessesNotInMemory);
+    print('pageFaultExceptionsThrown=' + objStatus.recordStats.pageFaultExceptionsThrown);
+  }
 
+}
