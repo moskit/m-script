48cf06116f4b43bde5b0fe399edb91e6e82357a6
Igor Simonov <me@igorsimonov.com>
2015-11-01 18:41:15 +0100

    fix

--- standalone/MongoDB/serverstatus.mongos.js
+++ standalone/MongoDB/serverstatus.mongos.js
@@ -1,27 +1,27 @@
 var objStatus = db.serverStatus()
-print('status=' + objStatus.ok);
-print('version=' + objStatus.version);
-print('uptime=' + objStatus.uptime);
-print('memRes=' + objStatus.mem.resident);
-print('memVir=' + objStatus.mem.virtual);
-print('memMapped=' + objStatus.mem.mapped);
-print('memMappedWJournal=' + objStatus.mem.mappedWithJournal);
-print('infoHeapUsage=' + objStatus.extra_info.heap_usage_bytes);
+if (objStatus.ok == 1) {
+  print('version=' + objStatus.version);
+  print('uptime=' + objStatus.uptime);
+  print('memRes=' + objStatus.mem.resident);
+  print('memVir=' + objStatus.mem.virtual);
+  print('memMapped=' + objStatus.mem.mapped);
+  print('memMappedWJournal=' + objStatus.mem.mappedWithJournal);
+  print('infoHeapUsage=' + objStatus.extra_info.heap_usage_bytes);
 
-print('connCurrent=' + objStatus.connections.current);
-print('connAvailable=' + objStatus.connections.available);
-if (objStatus.network) {
-  print('netIn=' + objStatus.network.bytesIn);
-  print('netOut=' + objStatus.network.bytesOut);
-  print('netReqn=' + objStatus.network.numRequests);
+  print('connCurrent=' + objStatus.connections.current);
+  print('connAvailable=' + objStatus.connections.available);
+  if (objStatus.network) {
+    print('netIn=' + objStatus.network.bytesIn);
+    print('netOut=' + objStatus.network.bytesOut);
+    print('netReqn=' + objStatus.network.numRequests);
+  }
+  if (objStatus.opcounters) {
+    print('opcountersInsert=' + objStatus.opcounters.insert);
+    print('opcountersQuery=' + objStatus.opcounters.query);
+    print('opcountersUpdate=' + objStatus.opcounters.update);
+    print('opcountersDelete=' + objStatus.opcounters.delete);
+    print('opcountersGetmore=' + objStatus.opcounters.getmore);
+    print('opcountersCommand=' + objStatus.opcounters.command);
+  }
 }
-if (objStatus.opcounters) {
-  print('opcountersInsert=' + objStatus.opcounters.insert);
-  print('opcountersQuery=' + objStatus.opcounters.query);
-  print('opcountersUpdate=' + objStatus.opcounters.update);
-  print('opcountersDelete=' + objStatus.opcounters.delete);
-  print('opcountersGetmore=' + objStatus.opcounters.getmore);
-  print('opcountersCommand=' + objStatus.opcounters.command);
-}
-
 
