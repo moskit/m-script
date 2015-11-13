f66d3d71f24a0c53ed8ea0a98cf02fcda5ea98ea
Igor Simonov <me@igorsimonov.com>
2015-11-13 23:54:51 +0100

    fix

--- lib/dash_functions.sh
+++ lib/dash_functions.sh
@@ -50,12 +50,11 @@ print_page_title() {
     dfpptclass=`echo "$dfpptarg" | cut -sd'|' -f2`
     [ -z "$dfpptclass" ] && dfpptclass="status"
     if [ "_$dfppttitle" == "_-" ]; then
-      echo "<div class=\"$dfpptclass\" id=\"$dfpptid\">&dash;</div>"
+      echo "<div class=\"$dfpptclass\" id=\"$dfpptid\">&nbsp;</div>"
       shift
       continue
     fi
-    dfpptcont=`eval echo \\$$dfppttitle`
-    echo "<div id=\"$dfpptid\" class=\"$dfpptclass\">${dfpptcont}</div>"
+    echo "<div id=\"$dfpptid\" class=\"$dfpptclass\"><b>${dfppttitle}</b></div>"
     shift
   done
   echo -e "  </div>\n</div>"
