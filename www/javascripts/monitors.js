f570261d5ed2c0317739826275c42d8e4d4c8bf9
Igor Simonov <me@igorsimonov.com>
2016-06-19 19:34:38 +0200

    fix

--- www/javascripts/monitors.js
+++ www/javascripts/monitors.js
@@ -123,12 +123,11 @@ showData = function(theid,base) {
   }
   server = $(theid).parentNode.id
   serverA = server.split("|");
+  if (serverA[2]) {
+    cluster = serverA[2] + "/" + cluster;
+  }
   if (serverA[1]) {
-    if (serverA[2]) {
-      server = serverA[2] + "/" + serverA[1];
-    } else {
-      server = serverA[1];
-    }
+    server = serverA[1];
   }
   theidA = theid.split("|");
   if (theidA[1]) {
